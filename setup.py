b59b62b48abb0ca04ee6a538a809136e5c4d5701
Austen Novis <austennovis@AustennPremiseMBP.local>
2021-03-05 10:33:47 -0600

    add tests for python 3.8

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ REQUIRED = [
     "apispec",
     "click",
     "google-api-python-client",
-    "google-cloud-storage",
     "functions-framework",
     "marshmallow",
     "ruamel.yaml",
@@ -112,6 +111,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
