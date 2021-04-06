5b8b0a9e875f1cc0bb891b98c4acdbb55085227f
Austen <anovis@ymail.com>
2021-04-06 12:31:56 -0400

    Bugfix for RequestBody and formData (#31)
    * fix requestBody and formData to work with openapispec 2

--- goblet/tests/test_openapispec.py
+++ goblet/tests/test_openapispec.py
@@ -114,8 +114,15 @@ class TestOpenApiSpec:
         assert response['400'] == {'description': '400'}
 
     def test_request_body(self):
-        route = RouteEntry(dummy, "route", "/home", "GET", request_body={'application/json': {'schema': {"type": "array", "items": {'type': 'string'}}}})
+        route = RouteEntry(dummy, "route", "/home", "GET", request_body={'schema': {"type": "array", "items": {'type': 'string'}}})
         spec = OpenApiSpec("test", "xyz.cloudfunction")
         spec.add_route(route)
-        request_body = spec.spec['paths']['/home']['get']['requestBody']
-        assert request_body == {'application/json': {'schema': {"type": "array", "items": {'type': 'string'}}}}
+        params = spec.spec['paths']['/home']['get']['parameters'][0]
+        assert params == {'in': 'body', 'name': 'requestBody', 'schema': {'type': 'array', 'items': {'type': 'string'}}}
+
+    def test_form_data(self):
+        route = RouteEntry(dummy, "route", "/home", "GET", form_data=True)
+        spec = OpenApiSpec("test", "xyz.cloudfunction")
+        spec.add_route(route)
+        params = spec.spec['paths']['/home']['get']['parameters'][0]
+        assert params == {'in': 'formData', 'name': 'file', 'type': 'file'}
