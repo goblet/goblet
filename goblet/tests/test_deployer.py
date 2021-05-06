89412c67b46a134d0d8af42872d6e1e35609ac0b
anovis <anovis@ymail.com>
2021-05-05 22:00:58 -0400

    add iam bindings

--- goblet/tests/test_deployer.py
+++ goblet/tests/test_deployer.py
@@ -20,7 +20,7 @@ class TestDeployer:
         Deployer().deploy(app, only_function=True)
 
         responses = get_responses('deployer-function-deploy')
-        assert(len(responses) == 4)
+        assert(len(responses) == 3)
 
     def test_destroy_http_function(self, monkeypatch):
         monkeypatch.setenv("GOOGLE_PROJECT", "goblet")
@@ -36,3 +36,20 @@ class TestDeployer:
         assert(len(responses) == 1)
         assert(responses[0]['body']['metadata']['type'] == 'DELETE_FUNCTION')
         assert(responses[0]['body']['metadata']['target'] == "projects/goblet/locations/us-central1/functions/goblet_test_app")
+
+    def test_set_iam_bindings(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_PROJECT", "goblet")
+        monkeypatch.setenv("GOOGLE_LOCATION", "us-central1")
+        monkeypatch.setenv("GOBLET_TEST_NAME", "deployer-function-bindings")
+        monkeypatch.setenv("GOBLET_HTTP_TEST", "REPLAY")
+
+        app = Goblet(function_name="goblet_bindings")
+        setattr(app, "entrypoint", 'app')
+
+        app.handlers['http'] = HTTP(dummy_function)
+        bindings = [{"role": "roles/cloudfunctions.invoker", "members": ["allUsers"]}]
+        Deployer().deploy(app, only_function=True, config={'bindings': bindings})
+
+        responses = get_responses('deployer-function-bindings')
+        assert(len(responses) == 4)
+        assert(responses[2]['body']['bindings'] == bindings)
