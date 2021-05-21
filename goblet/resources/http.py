26d331c4963f4dc15c186a13816ea150716ce8c7
anovis <anovis@ymail.com>
2021-05-20 20:26:49 -0400

    header filters on http endpoint

--- goblet/resources/http.py
+++ goblet/resources/http.py
@@ -4,17 +4,23 @@ from goblet.handler import Handler
 class HTTP(Handler):
     """Http Trigger"""
     def __init__(self, http=None):
-        self.http = http
+        self.http = http or []
 
-    def register_http(self, func):
-        self.http = func
+    def register_http(self, func, kwargs):
+        self.http.append({"func": func, "headers": kwargs.get("headers", {})})
 
     def __call__(self, request, context=None):
-        return self.http(request)
+        headers = request.headers or {}
+        for http_endpoint in self.http:
+            endpoint_headers = http_endpoint["headers"]
+            if isinstance(endpoint_headers, dict) and endpoint_headers.items() <= headers.items():
+                return http_endpoint["func"](request)
+            if isinstance(endpoint_headers, set) and endpoint_headers <= headers.keys():
+                return http_endpoint["func"](request)
 
     def __add__(self, other):
         if other.http:
-            self.http = other.http
+            self.http.extend(other.http)
         return self
 
     def deploy(self, sourceUrl=None, entrypoint=None):
--- goblet/tests/test_http.py
+++ goblet/tests/test_http.py
@@ -21,3 +21,52 @@ class TestHttp():
         app(mock_request, None)
 
         assert(mock.call_count == 1)
+
+    def test_call_headers_dict(self):
+        app = Goblet(function_name="goblet_example")
+        mock = Mock()
+
+        @app.http(headers={"test": 1})
+        def mock_function(request):
+            mock()
+            return True
+
+        mock_request = Mock()
+        mock_request.path = '/'
+        mock_request.headers = {"test": 1}
+
+        mock_request2 = Mock()
+        mock_request2.path = '/'
+        mock_request2.headers = {"test": 2}
+
+        app(mock_request, None)
+        app(mock_request2, None)
+
+        assert(mock.call_count == 1)
+
+    def test_call_headers_set(self):
+        app = Goblet(function_name="goblet_example")
+        mock = Mock()
+
+        @app.http(headers={"test"})
+        def mock_function(request):
+            mock()
+            return True
+
+        mock_request = Mock()
+        mock_request.path = '/'
+        mock_request.headers = {"test": 1}
+
+        mock_request2 = Mock()
+        mock_request2.path = '/'
+        mock_request2.headers = {"test": 2}
+
+        mock_request3 = Mock()
+        mock_request3.path = '/'
+        mock_request3.headers = {}
+
+        app(mock_request, None)
+        app(mock_request2, None)
+        app(mock_request3, None)
+
+        assert(mock.call_count == 2)
