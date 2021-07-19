169de885922b8a03d83a78a16f5513c6aa7d5218
Austen <anovis@ymail.com>
2021-07-19 16:52:56 -0400

    CORS (#74)
    Cors can be set on the route level or on the Goblet application level. Setting `cors=True` uses the default cors setting. (closes #60 )
    
    ```
    app=goblet("example",cors=True)
    ```
    or
    
    ```   
        @app.route('/custom_backend', cors=True)
        def home():
            return "cors headers"
    ```
    
    results in these headers 
    ```
        headers : {
            "Access-Control-Allow-Headers" : ['Content-Type', 'Authorization'],
            "Access-Control-Allow-Origin": "*"
        }
    ```
    Use the `CORSConfig` class to set customized cors headers from the `goblet.resources.routes` class. 
    
    ```
        from goblet.resources.routes import CORSConfig
        @app.route('/custom_cors', cors=CORSConfig(allow_origin='localhost'))
        def custom_cors():
            return jsonify('localhost is allowed')
    ```

--- goblet/app.py
+++ goblet/app.py
@@ -13,9 +13,9 @@ class Goblet(Register_Handlers):
     Main class which inherits most of its logic from the Register_Handlers class. Local param is used
     to set the entrypoint for running goblet locally
     """
-    def __init__(self, function_name="goblet", local=None):
+    def __init__(self, function_name="goblet", local=None, cors=None):
         self.function_name = GConfig().function_name or function_name
-        super(Goblet, self).__init__(function_name=self.function_name)
+        super(Goblet, self).__init__(function_name=self.function_name, cors=cors)
         self.log = logging.getLogger(__name__)
         self.headers = {}
         self.g = G()
