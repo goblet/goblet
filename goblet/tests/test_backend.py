988dedca1556a6f6140c63584c2f3f74d42738ad
anovis <anovis@ymail.com>
2022-12-12 09:59:03 -0500

    Add set-env flag to goblet local command

--- goblet/tests/test_backend.py
+++ goblet/tests/test_backend.py
@@ -1,5 +1,6 @@
 from goblet import Goblet
 from goblet.backends.backend import Backend
+from goblet.backends import CloudFunctionV1, CloudFunctionV2, CloudRun
 
 
 class TestBackend:
@@ -15,3 +16,28 @@ class TestBackend:
         assert "*.py" in backend.zip_config["include"]
         assert ".secret" in backend.zip_config["exclude"]
         assert "build" in backend.zip_config["exclude"]
+
+    def test_get_env_cloudfunction_v1(self):
+
+        test_env = {"cloudfunction": {"environmentVariables": {"TEST": "VALUE"}}}
+        backend = CloudFunctionV1(Goblet(), config=test_env)
+
+        assert backend.get_environment_vars() == {"TEST": "VALUE"}
+
+    def test_get_env_cloudfunction_v2(self):
+
+        test_env = {
+            "cloudfunction": {
+                "serviceConfig": {"environmentVariables": {"TEST": "VALUE"}}
+            }
+        }
+        backend = CloudFunctionV2(Goblet(), config=test_env)
+
+        assert backend.get_environment_vars() == {"TEST": "VALUE"}
+
+    def test_get_env_cloudrun(self):
+
+        test_env = {"cloudrun_container": {"env": [{"name": "TEST", "value": "VALUE"}]}}
+        backend = CloudRun(Goblet(), config=test_env)
+
+        assert backend.get_environment_vars() == {"TEST": "VALUE"}
