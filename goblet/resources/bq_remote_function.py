a0aea40979fc0aac456e00b46d0858f639e9545e
Diego Diaz <diegodiazespinoza@gmail.com>
2022-12-19 12:40:05 -0300

    [ADD] resources and decorators

--- goblet/resources/bq_remote_function.py
+++ goblet/resources/bq_remote_function.py
@@ -70,7 +70,7 @@ class BigQueryRemoteFunction(Handler):
         query_request = {
             "language":language,
             "routineReference": routine_reference,
-            "routineType": "PROCEDURE",
+            "routineType": "SCALAR_FUNCTION",
             "arguments":arguments,
             # "returnType": return_type,
             "remoteFunctionOptions":remote_function_options
