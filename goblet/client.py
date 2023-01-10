3bf5316cb065bbdac0f13d73216ef29cb3f45047
Diego Diaz <diegodiazespinoza@gmail.com>
2023-01-10 19:05:46 -0300

    [ADD] resources and decorators

--- goblet/client.py
+++ goblet/client.py
@@ -351,7 +351,7 @@ class VersionedClients:
     def bigquery_routines(self):
         return Client(
             "bigquery",
-            self.client_versions.get("bigquery", "v2"),
+            self.client_versions.get("bigquery", "v1"),
             calls="routines",
             parent_schema="{project_id}",
         )
