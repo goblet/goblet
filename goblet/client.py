eef4967dbded8e6315f68b36b8ca3881d04c3a7c
Diego Diaz <diegodiazespinoza@gmail.com>
2023-01-10 19:10:08 -0300

    [ADD] resources and decorators

--- goblet/client.py
+++ goblet/client.py
@@ -334,8 +334,8 @@ class VersionedClients:
         return Client(
             "bigqueryconnection",
             self.client_versions.get("bigqueryconnection", "v1"),
-            calls="projects.locations.connections",
-            parent_schema="projects/{project_id}/locations/{location_id}",
+            calls="connections",
+            parent_schema="{project_id}",
         )
 
     @property
