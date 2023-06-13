b1c297fde5f41064752075469886124fe00658b4
Austen Novis <anovis@ymail.com>
2023-06-13 09:37:41 -0400

    Add jquery to sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.githubpages", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.githubpages", "sphinx.ext.napoleon", "sphinxcontrib.jquery"]
 
 exclude_patterns = []
 
