645f3bb32fa9829eacf0a947b855efbcd465bc94
austen <anovis@ymail.com>
2025-08-28 21:49:39 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ REQUIRED = [
     "google-cloud-logging",
     "google-cloud-appengine-logging",
     "goblet-gcp-client",
-    "protobuf==4.24.*",
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
